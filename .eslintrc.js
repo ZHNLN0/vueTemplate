5456519dce36cb7d24c6d73fafab9ad1b5cb6d43
ZHNLN0 <779602661@qq.com>
2020-01-02 20:25:48 +0800

    add vscode-setting.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,147 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    es6: true,
+    browser: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  // extends: ['plugin:vue/essential', '@vue/prettier'],
+  extends: ['plugin:vue/recommended', 'eslint:recommended'],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/no-v-html': 'off',
+    "vue/max-attributes-per-line": ["error", {
+      "singleline": 5,
+      "multiline": {
+        "max": 2,
+        "allowFirstLine": false
+      }
+    }],
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/name-property-casing': ['error', 'PascalCase'],
+    'arrow-spacing': [
+      2,
+      {
+        'before': true,
+        'after': true
+      }
+    ],
+    'array-bracket-spacing': [2, 'never'],
+    'block-spacing': [2, 'always'],
+    'brace-style': [
+      2,
+      '1tbs',
+      {
+        'allowSingleLine': true
+      }
+    ],
+    'comma-dangle': [2, 'never'],
+    'comma-spacing': [
+      2,
+      {
+        'before': false,
+        'after': true
+      }
+    ],
+    'comma-style': [2, 'last'],
+    'constructor-super': 2,
+    'curly': [2, 'multi-line'],
+    'dot-location': [2, 'property'],
+    'eqeqeq': [
+      2,
+      'always',
+      {
+        'null': 'ignore'
+      }
+    ],
+    'handle-callback-err': [2, '^(err|error)$'],
+    'indent': [
+      2,
+      2,
+      {
+        'SwitchCase': 1
+      }
+    ],
+    'jsx-quotes': [2, 'prefer-single'],
+    'key-spacing': [
+      2,
+      {
+        'beforeColon': false,
+        'afterColon': true
+      }
+    ],
+    'new-cap': [
+      2,
+      {
+        'newIsCap': true,
+        'capIsNew': false
+      }
+    ],
+    'new-parens': 2,
+    'no-array-constructor': 2,
+    'no-caller': 2,
+    'no-const-assign': 2,
+    'no-delete-var': 2,
+    'no-dupe-args': 2,
+    'no-dupe-class-members': 2,
+    'no-dupe-keys': 2,
+    'no-duplicate-case': 2,
+    'no-eval': 2,
+    'no-ex-assign': 2,
+    'object-curly-spacing': [
+      2,
+      'always',
+      {
+        'objectsInObjects': false
+      }
+    ],
+    'semi': [2, 'never'],
+    'semi-spacing': [
+      2,
+      {
+        'before': false,
+        'after': true
+      }
+    ],
+    'space-before-blocks': [2, 'always'],
+    'space-before-function-paren': [2, 'never'],
+    'space-in-parens': [2, 'never'],
+    'space-infix-ops': 2,
+    'spaced-comment': [
+      2,
+      'always',
+      {
+        'markers': [
+          'global',
+          'globals',
+          'eslint',
+          'eslint-disable',
+          '*package',
+          '!',
+          ','
+        ]
+      }
+    ],
+    'template-curly-spacing': [2, 'never'],
+    'use-isnan': 2,
+    'valid-typeof': 2,
+    'yoda': [2, 'never'],
+    'prefer-const': 2
   },
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   },
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
       ],
       env: {
         jest: true
       }
     }
   ]
-};
+}
